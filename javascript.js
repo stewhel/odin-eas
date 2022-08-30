63f5a5fbf944d421a49d7bf7f6d12b749bc488c3
Stephen H <stewhel9@gmail.com>
2022-08-30 19:47:41 -0400

    Added CSS, improved html stlying, added slider input

--- javascript.js
+++ javascript.js
@@ -1,40 +1,71 @@
+//create initial board
 
-function colorBox(){
-    console.log("yo")
-}
+let askSquares;
+num = prompt("How many squares?")
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', makePad)
+
+const clearBtn = document.querySelector('.clear-btn')
+clearBtn.addEventListener('click', clearPad)
+
+const remBtn = document.querySelector('.rem-btn')
+remBtn.addEventListener('click', removeBoxes)
+
+const container = document.querySelector('.container')
+
 
 function makePad(){
-    for(let i=0; i<10; i++){
+    
+    removeBoxes(container);
+    
+    for(let i=0; i<(num*num); i++){
+        
+        size = (1/num) * 100;
+        let boxHeight = `height: ${size}%`;
+        let boxWidth = `width: ${size}%`;
+
+
         const div = document.createElement('div');
         div.classList.add('box');
-        div.textContent = "not empy";
+        div.setAttribute('id', i)
+        div.style.cssText = boxHeight;
+        div.style.cssText = boxWidth;
+        div.onmouseover = colorBox; 
+        
         const container = document.querySelector('.container');
         container.appendChild(div);
+    }
+
+}
+
+
+function removeColor(box){
+    box.style.backgroundColor= 'rgb(255, 255, 255)'
+}
+
+function clearPad(){
+    let boxes = document.querySelectorAll('.box')
+    boxes.forEach(box => removeColor(box))
+}
+
+function colorBox(e){
+    //console.log(e.path[0])
+    box = e.path[0]
+    box.style.backgroundColor='rgba(0, 0, 255, 1)'
+}
+
+function removeBoxes(parent){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
 }
 
 
 
 
-function addHover(){
-    let boxes = document.querySelectorAll('.box');
-    console.log(boxes)
-    boxes.forEach(box => box.addEventListener('click', colorBox));
-}
 
 
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', makePad)
-
-const btn2 = document.querySelector('.btn2')
-btn2.addEventListener('click', addHover)
-
-document.getElementsByClassName('.container2').onclick = colorBox'
-
-
-
-
-//const color = document.querySelector('.box');
 
 
 
