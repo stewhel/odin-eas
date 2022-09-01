7f0a7969c3ada8729a58a856d86d5a1bef90f0e7
Stephen H <stewhel9@gmail.com>
2022-08-31 20:28:10 -0400

    Clean version of EAS project

--- javascript.js
+++ javascript.js
@@ -1,44 +1,44 @@
-//create initial board
-
-let askSquares;
-num = prompt("How many squares?")
-
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', makePad)
 
 const clearBtn = document.querySelector('.clear-btn')
 clearBtn.addEventListener('click', clearPad)
 
-const remBtn = document.querySelector('.rem-btn')
-remBtn.addEventListener('click', removeBoxes)
+const changeBtn = document.querySelector('.change-btn')
+changeBtn.addEventListener('click', test)
 
 const container = document.querySelector('.container')
 
+const slider = document.querySelector('.slider')
+slider.addEventListener('mouseup', makePad)
+
+let color = 'rgba(0, 0, 0, .5)'
+
+function test(){
+    console.log(slider.value)
+}
 
 function makePad(){
     
     removeBoxes(container);
+
+    let sliderVal = document.getElementById('myRange')
+    let num = sliderVal.value;
     
     for(let i=0; i<(num*num); i++){
         
         size = (1/num) * 100;
         let boxHeight = `height: ${size}%`;
         let boxWidth = `width: ${size}%`;
-
-
         const div = document.createElement('div');
         div.classList.add('box');
         div.setAttribute('id', i)
         div.style.cssText = boxHeight;
         div.style.cssText = boxWidth;
         div.onmouseover = colorBox; 
-        
         const container = document.querySelector('.container');
         container.appendChild(div);
     }
 
 }
-
 
 function removeColor(box){
     box.style.backgroundColor= 'rgb(255, 255, 255)'
@@ -50,9 +50,8 @@ function clearPad(){
 }
 
 function colorBox(e){
-    //console.log(e.path[0])
     box = e.path[0]
-    box.style.backgroundColor='rgba(0, 0, 255, 1)'
+    box.style.backgroundColor= color
 }
 
 function removeBoxes(parent){
@@ -61,8 +60,7 @@ function removeBoxes(parent){
     }
 }
 
-
-
+window.onload = makePad;
 
 
 
